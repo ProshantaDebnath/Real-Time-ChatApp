7b77a73e125aabd3bb4511a1a6d6989bc9bec694
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2021-09-13 22:53:50 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h3>Real Time Chat-App</h3>
+
+    </div>
+  );
+}
+
+export default App;
